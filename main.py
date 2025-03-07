eb7db4db0b506b3e70b08ded9dce2a3f699c033f
Aaron Karr <aaron@aaronkarr.com>
2025-03-07 13:39:50 -0500

    point and line objects

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from graphics import Window
+from graphics import Point, Window, Line
 
 
 def main():
     window = Window(800, 600)
+
+    p1 = Point(50, 50)
+    p2 = Point(400, 200)
+    line = Line(p1, p2)
+
+    window.draw_line(line, fill_color="red")
+
     window.wait_for_close()
 
 if __name__ == "__main__":
     main()
-
 
