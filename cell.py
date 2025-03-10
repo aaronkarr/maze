e442860386d8e16a65ecf2ea07d1f3cb412f8f80
Aaron Karr <aaron@aaronkarr.com>
2025-03-10 16:31:18 -0400

    line for maze moves

--- cell.py
+++ cell.py
@@ -21,14 +21,26 @@ class Cell:
         self.__y2 = y2
 
         if self.has_left_wall:
-            wall = Line(Point(x1, y1), Point(x1, y2))
-            self.__window.draw_line(wall)
+            left_wall = Line(Point(x1, y1), Point(x1, y2))
+            self.__window.draw_line(left_wall)
         if self.has_right_wall:
-            wall = Line(Point(x2, y1), Point(x2, y2))
-            self.__window.draw_line(wall)
+            right_wall = Line(Point(x2, y1), Point(x2, y2))
+            self.__window.draw_line(right_wall)
         if self.has_top_wall:
-            wall = Line(Point(x1, y1), Point(x2, y1))
-            self.__window.draw_line(wall)
-        if self.has_left_wall:
-            wall = Line(Point(x1, y2), Point(x2, y2))
-            self.__window.draw_line(wall)
+            top_wall = Line(Point(x1, y1), Point(x2, y1))
+            self.__window.draw_line(top_wall)
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(x1, y2), Point(x2, y2))
+            self.__window.draw_line(bottom_wall)
+
+    def draw_move(self, to_cell, undo=False):
+        move_x = (self.__x1 + self.__x2) // 2
+        move_y = (self.__y1 + self.__y2) // 2
+        move_to_x = (to_cell.__x1 + to_cell.__x2) / 2
+        move_to_y = (to_cell.__y1 + to_cell.__y2) / 2
+        move_line = Line(Point(move_x, move_y), Point(move_to_x, move_to_y))
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        self.__window.draw_line(move_line, fill_color=fill_color)
--- main.py
+++ main.py
@@ -4,15 +4,30 @@ from cell import Cell
 def main():
     window = Window(800, 600)
 
-    # p1 = Point(50, 50)
-    # p2 = Point(400, 200)
-    # line = Line(p1, p2)
-    #
-    # window.draw_line(line, fill_color="red")
+    c1 = Cell(window)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    cell = Cell(window)
-    cell.draw(100, 200, 300, 400)
-    cell.draw(200, 300, 400, 500)
+    c2 = Cell(window)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(window)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(window)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
 
     window.wait_for_close()
 
