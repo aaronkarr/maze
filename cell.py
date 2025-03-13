858fee669535e1ad0771efbac838ac9784ba6d37
Aaron Karr <aaron@aaronkarr.com>
2025-03-12 22:41:13 -0400

    maze object

--- cell.py
+++ cell.py
@@ -15,6 +15,8 @@ class Cell:
         self.__window = window
 
     def draw(self, x1, y1, x2, y2):
+        if self.__window is None:
+            return
         self.__x1 = x1
         self.__x2 = x2
         self.__y1 = y1
