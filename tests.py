8ea9c95521b0c4d62b51b359424cb9148e9c2734
Aaron Karr <aaron@aaronkarr.com>
2025-03-14 18:58:58 -0400

    create random maze and tests

--- tests.py
+++ tests.py
@@ -15,5 +15,29 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_break_entrance_and_exit(self):
+        num_rows = 15
+        num_cols = 20
+        maze2 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            maze2._cells[0][0].has_top_wall,
+            False
+        )
+        self.assertEqual(
+            maze2._cells[num_cols - 1][num_rows -1].has_bottom_wall,
+            False
+        )
+
+    def test_maze_reset_visited(self):
+        num_rows = 10
+        num_cols = 13
+        maze3 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in maze3._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False
+                )
+
 if __name__ == "__main__":
     unittest.main()
